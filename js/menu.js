b04afe63a5bfebdcd3c443faa0494663e9341593
Rossana <rossana_naki@hotmail.com>
2018-02-09 15:44:28 +0000

    Menu atualizado

--- js/menu.js
+++ js/menu.js
@@ -1,62 +1,36 @@
 $(document).ready(function () {
-  var slideout = new Slideout({
-    'panel': document.getElementsByClassName('main')[0],
-    'menu': document.getElementsByClassName('menu-mobile')[0],
-    'padding': 256,
-    'tolerance': 70
+  var indexColor;
+  $('.navbar-mobile .botao-mobile').click(function () {
+
+    var menu = $('.menu-mobile .topicosMenu');
+    if (menu.is(":visible")){
+      $('html').css({"overflow": "initial"});
+      menu.slideToggle("slow", function () {
+        //$('.index .zonaMenu').css("background-color", indexColor);
+        $('.index .zonaMenu').animate({backgroundColor: indexColor}, 'slow');
+      });
+    }
+
+    else {
+      indexColor = $('.index .zonaMenu').css("background-color");
+      menu.slideToggle("slow", function () {});
+        $('.index .zonaMenu').css("background-color", "rgba(6, 41, 64, 1)");
+      $('html').css({"overflow": "hidden"});
+    }
   });
 
-  document.querySelector('.botao-mobile').addEventListener('click', function() {
-    slideout.toggle();
-  });
-
-  slideout.on('beforeopen', function () {
-    $('.main')
-      .css('filter','brightness(0.4)')
-      .css('webkitFilter','brightness(0.4)')
-      .css('mozFilter','brightness(0.4)')
-      .css('oFilter','brightness(0.4)')
-      .css('msFilter','brightness(0.4)');
-  });
-
-  slideout.on('beforeclose', function () {
-    $('.main')
-      .css('filter','none')
-      .css('webkitFilter','none')
-      .css('mozFilter','none')
-      .css('oFilter','none')
-      .css('msFilter','none');
-  });
-
-  var fixed = document.querySelector('.zonaMenu');
-
-  slideout.on('translate', function(translated) {
-    fixed.style.transform = 'translateX(' + translated + 'px)';
-  });
-
-  slideout.on('beforeopen', function () {
-    fixed.style.transition = 'transform 300ms ease';
-    fixed.style.transform = 'translateX(256px)';
-  });
-
-  slideout.on('beforeclose', function () {
-    fixed.style.transition = 'transform 300ms ease';
-    fixed.style.transform = 'translateX(0px)';
-  });
-
-  slideout.on('open', function () {
-    fixed.style.transition = '';
-  });
-
-  slideout.on('close', function () {
-    fixed.style.transition = '';
-  });
-
-
-  $('.submenus').click(function(){
-    if($('#dropmenu').is(":visible"))
-      $('#dropmenu').hide();
-    else
-      $('#dropmenu').show();
-  });
+  $('.menu-mobile button').click(function() {
+    var menu = $('.menu-mobile .topicosMenu');
+    $('html').css({"overflow": "initial"});
+    menu.slideToggle("slow", function () {
+      $('.index .zonaMenu').css("background-color", indexColor);
+    });
+  })
+  $(window).scroll(function(){
+    scrollTop = $(window).scrollTop();
+      if (scrollTop >= 100)
+        $('.index .zonaMenu').css("background-color", "rgba(6, 41, 64, 1)");
+      else
+        $('.index .zonaMenu').css("background-color", "rgba(6, 41, 64, " + (scrollTop/100) + ")");
+    });
 });
