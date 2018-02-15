9a352b5c431a88eb990a12e4d5a62eac4161066b
João Madeira <jprm1997@hotmail.com>
2018-02-15 16:48:52 +0000

    bugs de animacoes e posição dos link uteis/documentos uteis

--- js/getTemas.js
+++ js/getTemas.js
@@ -152,14 +152,14 @@ function addVideo(link, titulo, texto) {
 function addLink(classe, nome, url) {
     var imagem = "imagens/link_" + classe.toLowerCase() + ".png";
     var link = `<a href="${url}" class="wow fadeIn"><p class="linkUteis hvr-overline-from-left"><img src="${imagem}">${nome}</p></a>
-        <br>`;
+        <br><br>`;
     $('#zonaLinks').append(link);
 } //addLink
 
 function addDocumento(classe, nome, url) {
     var imagem = "imagens/pin_" + classe.toLowerCase() + ".png";
     var documento = `<a href="${url}" class="wow fadeIn"> <p class="documentos hvr-overline-from-left"><img src="${imagem}">${nome}</p></a>
-            <br>`;
+            <br><br>`;
     $('#zonaDocumentos').append(documento);
 } //addDocumento
 
